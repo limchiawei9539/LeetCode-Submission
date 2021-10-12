3de3609035f54b4c674eee0b38a7d4ba3a239b69
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-10-12 18:17:48 +0800

    -Completed 35

---
+++ 35. Search Insert Position/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _35._Search_Insert_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            while (low <= high)
+            {
+                var mid = low + (high - low) / 2;
+                if(target == nums[mid])
+                {
+                    return mid;
+                }
+                else if (target > nums[mid])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+            return low;
+        }
+    }
+}
