0d3ecc31253efe925011794485e8ed4f6c71d8ad
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-10-12 17:59:30 +0800

    -Completed 278,704

---
+++ 278. First Bad Version/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _278._First_Bad_Version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int FirstBadVersion(int n)
+        {
+            int low = 1;
+            int high = n;
+            while (low <= high)
+            {
+                var mid = low + (high - low) / 2;
+                var res = IsBadVersion(mid);
+                if (res)
+                {
+                    if (IsBadVersion(mid + 1))
+                    {
+                        high = mid - 1;
+                    }
+                    else if (!IsBadVersion(mid + 1))
+                    {
+                        return mid;
+                    }
+                }
+                else if (!res)
+                {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+
+        //---ignore code below---//
+        public static bool IsBadVersion(int n)
+        {
+            return true;
+        }
+    }
+}
---
+++ 704. Binary Search/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _704._Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            var res = -1;
+            int low = 0;
+            int high = nums.Length - 1;
+            if(!Array.Exists(nums,t => t == target))
+            {
+                return -1;
+            }
+            while (low <= high)
+            {
+                var mid = low + (high - low) / 2;
+                res = nums[mid];
+                if (res == target)
+                {
+                    return mid;
+                }
+                else if (res > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return res;
+        }
+    }
+}
