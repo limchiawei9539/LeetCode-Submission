e6ac9e15ee7cc14941d19948007b147acfe6d59b
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-10-13 12:10:32 +0800

    Completed 977,189

---
+++ 189. Rotate Array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _189._Rotate_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rotate(new int[7] { 1, 2, 3, 4, 5, 6, 7 }, 3);
+        }
+
+        public static void Rotate(int[] nums, int k)
+        {
+            if (nums.Length <= 1 || nums == null || k == 0 || nums.Length == k)
+            {
+                return;
+            }
+            int i = 0;
+            int[] arr = new int[nums.Length];
+            int idx = nums.Length - (k % nums.Length);
+            while (i < nums.Length)
+            {
+                arr[i] = nums[idx];
+                if (idx == nums.Length - 1)
+                {
+                    idx = 0;
+                }
+                else
+                {
+                    idx++;
+                }
+                i++;
+            }
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                nums[j] = arr[j];
+            }
+        }
+    }
+}
---
+++ 977. Squares of a Sorted Array/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _977._Squares_of_a_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SortedSquares(new int[5] { -4, -1, 0, 3, 10 }));
+        }
+
+        public static int[] SortedSquares(int[] nums)
+        {
+            int i = 0;
+            int j = nums.Length - 1;
+            int current = nums.Length - 1;
+            int[] arr = new int[nums.Length];
+
+            if(nums.Length==1)
+            {
+                arr[0] = nums[0] * nums[0];
+                return arr;
+            }
+
+            while (i < j)
+            {
+                if(Math.Abs(nums[i]) > Math.Abs(nums[j]))
+                {
+                    arr[current] = nums[i] * nums[i];
+                    i++;
+                }
+                else
+                {
+                    arr[current] = nums[j] * nums[j];
+                    j--;
+                }
+                current--;
+            }
+            return arr;
+        }
+    }
+}
