038c54c19d6482e99b7d5563b5dd1d3b82ad13a9
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-10-15 11:04:45 +0800

    Completed 309

---
+++ 309. Best Time to Buy and Sell Stock with Cooldown/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _309._Best_Time_to_Buy_and_Sell_Stock_with_Cooldown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProfit(new int[5] { 1, 2, 3, 0, 2 }));
+            Console.WriteLine(MaxProfit(new int[3] { 1, 3, 4 }));
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int rest = 0; //do nothing
+            int hold = rest - prices[0]; //buy today
+            int sold = hold + prices[0]; // sell today
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int B4Sold = sold; //yesterday sold
+                sold = hold + prices[i]; // sell today
+                hold = Math.Max(hold, rest - prices[i]); //holding or buy today
+                rest = Math.Max(rest, B4Sold); //resting or sell yesterday 
+            }
+            return Math.Max(sold,rest);
+        }
+    }
+}
