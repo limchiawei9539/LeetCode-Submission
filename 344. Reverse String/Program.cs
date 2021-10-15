82dcbfbeecc94edc4151bd42d86177e4d2bb162d
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-10-15 11:55:38 +0800

    Complete 334,557

---
+++ 344. Reverse String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _344._Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseString(new char[5] { 'h', 'e', 'l', 'l', 'o' });
+        }
+
+        public static void ReverseString(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            while (i <= j)
+            {
+                var temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+                i++;
+                j--;
+            }
+        }
+    }
+}
---
+++ 557. Reverse Words in a String III/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace _557._Reverse_Words_in_a_String_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("Let's take LeetCode contest"));
+        }
+
+        public static string ReverseWords(string s)
+        {
+            var arr = new StringBuilder(s);
+            int i = 0;
+            int j = 0;
+            var first = 0;
+
+            for (int l = 0; l < arr.Length; l++)
+            {
+                if (arr[l] == ' ' || l == s.Length - 1)
+                {
+                    j = l == s.Length - 1 ? l : l - 1;
+                    i = first;
+                    while (i < j)
+                    {
+                        var temp = s[i];
+                        arr[i] = s[j];
+                        arr[j] = temp;
+                        i++;
+                        j--;
+                    }
+                    first = l + 1;
+                }
+            }
+            return arr.ToString();
+        }
+    }
+}
