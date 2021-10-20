605bc77af45879666a4808ff74ec3ffd5c574f5f
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-10-20 09:42:34 +0800

    Completed 151

---
+++ 151. Reverse Words in a String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _151._Reverse_Words_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWords("    the sky  is    blue"));
+        }
+
+        public static string ReverseWords(string s)
+        {
+            s = s.Trim();
+            var result = "";
+            var arr = s.Split(' ');
+            for (int i = arr.Length-1; i >=0; i--)
+            {
+                if (!String.IsNullOrWhiteSpace(arr[i]))
+                {
+                    result += arr[i];
+                    if (i > 0)
+                    {
+                        result += " ";
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
