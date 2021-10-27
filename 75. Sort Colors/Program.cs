34c9c739b68c65da0cc43210ad474797f74764d0
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-10-27 11:34:05 +0800

    Completed 75

---
+++ 75. Sort Colors/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _75._Sort_Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortColors(new int[6] { 2, 0, 2, 1, 1, 0 });
+        }
+
+        public static void SortColors(int[] nums)
+        {
+            if (nums.Length <= 1 || nums == null)
+            {
+                return;
+            }
+            int idx = 0;
+            int x = 0;
+            int y = nums.Length - 1;
+            while (idx <= y)
+            {
+                if (nums[idx] == 0)
+                {
+                    var temp = nums[x];
+                    nums[x++] = nums[idx];
+                    nums[idx++] = temp;
+                }
+                else if (nums[idx] == 2)
+                {
+                    var temp = nums[y];
+                    nums[y--] = nums[idx];
+                    nums[idx] = temp;
+                }
+                else
+                {
+                    idx++;
+                }
+
+                for(int i=0;i<nums.Length;i++)
+                {
+                    Console.WriteLine(nums[i]);
+                }
+            }
+        }
+    }
+}
